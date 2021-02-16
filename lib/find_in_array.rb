7a83fe6729dc757c006950ef13439ab524b00ded
Allen Palacios <apalacios1994@gmail.com>
2021-02-16 15:57:56 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -9,12 +9,6 @@ def find_element_index(array, value_to_find)
   zig_zag = [500,4,1000,5,250]
   count = 0
   found_value_index = nil
-  
-  while count < array.length do
-    if array[count] == value_to_find
-      found_value_index = count
-    end
-    count += 1
-  end
-  found_value_index
+  binding.pry
+ array.max(array)
 end
